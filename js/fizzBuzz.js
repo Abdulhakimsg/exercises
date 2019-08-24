9df8a32516436aa4428081b22a60618569bc1b8c
Hakim <hakim@insightbrewers.com>
2019-08-24 15:07:18 +0800

    finalised fizzBuzz solution

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -11,7 +11,7 @@ function fizzBuzz(){
     // if i modulus of 3 & 5 print 
 
     for(let i = 1 ; i <=100 ; i++ ){
-        if(i% 3 === 0 && i%5 === 0){
+        if(i% 15 === 0){
             console.log('FizzBuzz')
         }
         else if (i % 3 === 0 ){
